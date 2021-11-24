3296f2e98a5c96e7753a8fc2c772b0feac999e1b
ChristianWalmar <edbuiskindaawesome@gmail.com>
2021-11-24 18:34:11 +0100

    HAve added some different repos and the LoginService class

---
+++ src/main/java/com/example/domain/services/LoginService.java
@@ -0,0 +1,43 @@
+package com.example.domain.services;
+
+import com.example.domain.models.User;
+import com.example.repositories.UserRepository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginService {
+  private final UserRepository userRepository = new UserRepository();
+
+  public boolean checkIfUserExists(User userEntered) { // checkIfUserExistsLogin
+    ResultSet rs = userRepository.dbRead();
+    try {
+      while (rs.next()) {
+        String userTempEmail = rs.getString(1);
+        String userTempPassword = rs.getString(2);
+        if ((userTempEmail).equals(userEntered.getEmail()) &&
+            (userTempPassword).equals(userEntered.getPassword())) {
+          return true;
+        }
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return false;
+  }
+
+  public boolean checkIfUserExistsRegister(User userEntered) {
+    ResultSet rs = userRepository.getAllUsersFromDB();
+    try {
+      while (rs.next()) {
+        String userTempEmail = rs.getString(1);
+        if ((userTempEmail).equals(userEntered.getEmail())) {
+          return true;
+        }
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return false;
+  }
+}
--- src/main/java/com/example/repositories/ProjectRepository.java
+++ src/main/java/com/example/repositories/ProjectRepository.java
@@ -1,4 +1,44 @@
 package com.example.repositories;
 
+import com.example.domain.models.Project;
+import com.example.domain.models.User;
+
+import java.sql.*;
+
 public class ProjectRepository {
+
+
+  //For inserting a project into the DB.
+  public void dbWrite(Project project) /*throws LoginSampleException*/ {
+    try {
+      Connection con = DBManager.getConnection();
+      String SQL = "INSERT INTO users (/*email, password, first_name, last_name, address, age, phone_number)" +
+          " VALUES (?, ?, ?, ?, ?, ?, ?*/)";
+      PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+      ps.setString(1, project.getProjectName());
+      ps.setString(2, project.getCategory());
+      ps.setInt(3, project.getHoursTotal());
+      ps.setString(4, project.getStartDate());
+      ps.setString(5, project.getEndDate());
+      ps.setString(6, project.getOwnerEmail());
+      ps.setString(7, project.getDescription());
+      ps.executeUpdate();
+    } catch (SQLException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  //For reading a project from DB
+  public ResultSet dbRead() {
+    ResultSet resSet = null;
+    String select = "/*SELECT email, password, first_name, last_name, age, address, phone_number FROM users*/";
+    try {
+      PreparedStatement ps = DBManager.getConnection().prepareStatement(select);
+      resSet = ps.executeQuery();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return resSet;
+  }
+
 }
