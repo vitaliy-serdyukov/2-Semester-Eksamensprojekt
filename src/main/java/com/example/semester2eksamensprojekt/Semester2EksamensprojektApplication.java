4cebe2f5a34dd5c2bd3530bd7a4b9a87a0b58a95
vital <vitaliy_serdyukov@hotmail.com>
2021-11-24 13:16:05 +0100

    initial commit

---
+++ src/main/java/com/example/semester2eksamensprojekt/Semester2EksamensprojektApplication.java
@@ -0,0 +1,13 @@
+package com.example.semester2eksamensprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semester2EksamensprojektApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Semester2EksamensprojektApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/semester2eksamensprojekt/Semester2EksamensprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.semester2eksamensprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semester2EksamensprojektApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
